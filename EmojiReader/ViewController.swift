a4b9966711527e7791942e02f9729953ff2a52a9
Max Pavlov <max.pavlov711@gmail.com>
2022-01-16 14:08:00 +0300

    Working UITabkeView

---
+++ EmojiReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Max Pavlov on 16.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
