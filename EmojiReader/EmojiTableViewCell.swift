118cb950bf20ea82355a332d6e9528c45e9080de
Max Pavlov <max.pavlov711@gmail.com>
2022-01-16 14:48:51 +0300

    UITableViewCell

---
+++ EmojiReader/EmojiTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by Max Pavlov on 16.01.22.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
