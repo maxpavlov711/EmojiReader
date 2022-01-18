48230444fc3e82d5a4539ca65e3d3d9db566474c
Max Pavlov <max.pavlov711@gmail.com>
2022-01-18 22:23:52 +0300

    New Functions Cells

--- EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -15,13 +15,33 @@ class NewEmojiTableViewController: UITableViewController {
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavorite: false)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateSaveButtonState()
     }
     
     @IBAction func textChanget(_ sender: Any) {
+        updateSaveButtonState()
     }
 
-
+    private func updateSaveButtonState() {
+        let emojiText = emojiTextFiend.text ?? ""
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descriptionTextField.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "save" else { return }
+        
+        let emoji = emojiTextFiend.text ?? ""
+        let name = nameTextField.text ?? ""
+        let description = descriptionTextField.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavorite: self.emoji.isFavorite)
+    }
 }
